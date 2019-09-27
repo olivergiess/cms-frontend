113db1520a7e7e0e2b26057c6b59f4240ce5b1e7
Oliver Giess <oliver@compassit.biz>
2019-09-27 14:20:52 +1000

    Initial commit.

---
+++ pages/posts/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="posts"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+   export default {
+    data: () => ({
+      table: {
+          headers: [
+              { text: 'Title' },
+              { text: 'Publish At' },
+          ],
+          posts: []
+      }
+    }),
+   }
+</script>
