8b60d28310546ee02a9e5fb14aeb03fbcb1ef6fe
Oliver Giess <oliver@compassit.biz>
2019-10-23 15:34:40 +1000

    Feature: Continued work on blog.

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,28 +1,43 @@
 <template>
-    <v-col
-      cols="12"
-      sm="10"
-      md="8"
-    >
-      {{ posts }}
-    </v-col>
+  <v-col
+    cols="12"
+    sm="10"
+    md="8"
+  >
+    {{ posts }}
+  </v-col>
 </template>
 
 <script>
-  import User from '@/models/User'
-  import Post from '@/models/Post'
+    import User from '@/models/User'
+    import Post from '@/models/Post'
 
-  export default {
-      auth: false,
-      components: {
-      },
-      async mounted() {
-          User.api().fetchWithPublished(this.$route.params.slug);
-      },
-      computed: {
-          posts() {
-              return User.query().where('slug', this.$route.params.slug).with('posts').get();
-          }
-      },
-  }
+    export default {
+        auth: false,
+        components: {},
+        async mounted() {
+            // let t = await User.api().fetchWithPublished(this.$route.params.slug);
+            User.insertOrUpdate({
+                    id: 1,
+                    first_name: "Dixie",
+                    last_name: "Pfannerstill",
+                    email: "test@test.com",
+                    slug: "test",
+                    posts: [{
+                        id: 1,
+                        title: "Not Today",
+                        body: "<p>Text<\/p>",
+                        publish_at: "2019-10-08",
+                        user_id: 1
+                    }]
+                }
+            );
+        },
+        computed: {
+            posts() {
+                return User.query().where('slug', this.$route.params.slug).with('posts').get();
+            }
+        }
+        ,
+    }
 </script>
